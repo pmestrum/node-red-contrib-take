b32efed47455d6afae1578e08536cf3acc6e8612
Paul Mestrum <paul.mestrum@delaware.pro>
2023-02-18 13:49:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Take Node
+
+## Usage
+
+See help in node-red on this node
---
+++ take.js
@@ -0,0 +1,67 @@
+module.exports = function (RED) {
+
+    const cooldown = (node, resetTimeout, maxTry) => {
+        const cooldownTime =  (node.startTime + resetTimeout * 1000) - new Date().getTime();
+        if (cooldownTime > 0) {
+            const fill = node.counter > maxTry ? 'red' : 'green';
+            node.status({ fill, shape: 'ring', text: `${node.counter}/${maxTry} - ${Math.round(cooldownTime / 1000)}s` });
+            node.subscription = setTimeout(() => cooldown(node, resetTimeout, maxTry), 1000);
+        } else {
+            node.counter = 0;
+            node.status({ fill: 'green', shape: 'dot', text: `0/${maxTry}` });
+            if (node.done) {
+                node.done();
+            }
+        }
+    }
+    function takeNode(config) {
+
+        RED.nodes.createNode(this, config);
+        const node = this;
+        const maxTry = config.attempts;
+        const resetTimeout = config.timeout;
+
+        node.status({ fill: 'green', shape: 'dot', text: `0/${maxTry}` });
+
+        node.on('input', (msg) => {
+
+            node.startTime = new Date().getTime();
+
+            if (node.subscription) {
+                clearTimeout(node.subscription);
+            }
+
+            if (msg?.resetTimer) {
+                node.counter = 0;
+                node.status({ fill: 'green', shape: 'dot', text: `0/${maxTry}` });
+            } else {
+                node.counter = node.counter || 0;
+                node.counter++;
+
+                if (node.counter > maxTry) {
+                    node.send([null, msg]);
+                    node.status({ fill: 'red', shape: 'dot', text: `${node.counter}/${maxTry} ${resetTimeout}s` });
+                } else {
+                    node.send([msg, null]);
+                    node.status({ fill: 'green', shape: 'ring', text: `${node.counter}/${maxTry} ${resetTimeout}s` });
+                }
+                node.subscription = setTimeout(() => cooldown(node, resetTimeout, maxTry), (resetTimeout % 1000) || 1000);
+            }
+
+            if (node.done) {
+                node.done();
+            }
+        });
+
+        node.on('close', (removed, done) => {
+            if (node.subscription) {
+                clearTimeout(node.subscription);
+            }
+            if (done && typeof done === 'function') {
+                done();
+            }
+        });
+    }
+
+    RED.nodes.registerType("take", takeNode);
+}
